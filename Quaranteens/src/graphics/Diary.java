fcebbee1e8e5c4259636f941a11d2d49955d1f3b
Sonia Muzemil <muzemils@wustl.edu>
2020-04-17 16:31:33 -0500

    created Diary class

---
+++ Quaranteens/src/graphics/Diary.java
@@ -0,0 +1,30 @@
+package graphics;
+
+import java.util.Date;
+
+public class Diary {
+	
+	private Date dateCreated;
+	private String contentOfEntry;
+	
+	
+	public Diary() {
+		
+	}
+	
+	public void addDiaryEntry() {
+		
+	}
+	
+	public void saveDiaryEntry() {
+		
+	}
+	
+	public void searchDiaryEntry() {
+		
+	}
+	
+	public void deleteDiaryEntry() {
+		
+	}
+}
