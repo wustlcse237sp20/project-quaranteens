4155a2856a287df5920ab8f34c228027d5289a38
williesu7 <suwilliam@wustl.edu>
2020-04-19 16:25:16 -0400

    code for now

--- Quaranteens/src/graphics/DiaryManager.java
+++ Quaranteens/src/graphics/DiaryManager.java
@@ -1,7 +1,24 @@
 package graphics;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class DiaryManager {
 	
+	private String path = "src/docs.DiaryEntries/sample.txt";
+	
+	
+	public void writeToFile() {
+		
+		File diaryEntries = new File(path);
+		try {
+			FileWriter writingDiaryEntries = new FileWriter(diaryEntries, true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	public void addDiaryEntry() {
 		
 	}
