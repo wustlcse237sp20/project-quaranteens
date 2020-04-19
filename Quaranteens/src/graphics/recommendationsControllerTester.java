76a7099365764c14c50dc1a66ffb24c58e61c78b
williesu7 <suwilliam@wustl.edu>
2020-04-19 15:59:57 -0400

    recommendationsTester Done

--- Quaranteens/src/graphics/recommendationsControllerTester.java
+++ Quaranteens/src/graphics/recommendationsControllerTester.java
@@ -7,8 +7,44 @@ import org.junit.jupiter.api.Test;
 class recommendationsControllerTester {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testReadMovieRecs() {
+		recommendationsController rc = new recommendationsController();
+		rc.readMovieRecFromFile();
+		assertFalse(rc.listOfMovies.isEmpty());
 	}
-
+	
+	void testGenerateMovieRecs() {
+		recommendationsController rc = new recommendationsController();
+		rc.readMovieRecFromFile();
+		String tip = rc.generateMovieRec();
+		System.out.println(tip);
+		assertEquals(String.class, tip.getClass());
+	}
+	void testReadBookRecs() {
+		recommendationsController rc = new recommendationsController();
+		rc.readBookRecFromFile();
+		assertFalse(rc.listOfBooks.isEmpty());
+	}
+	
+	void testGenerateBooksRecs() {
+		recommendationsController rc = new recommendationsController();
+		rc.readBookRecFromFile();
+		String tip = rc.generateBookRec();
+		System.out.println(tip);
+		assertEquals(String.class, tip.getClass());
+	}
+	
+	void testReadMusicRecs() {
+		recommendationsController rc = new recommendationsController();
+		rc.readMusicRecFromFile();
+		assertFalse(rc.listOfMusic.isEmpty());
+	}
+	
+	void testGenerateMusicRecs() {
+		recommendationsController rc = new recommendationsController();
+		rc.readBookRecFromFile();
+		String tip = rc.generateMusicRec();
+		System.out.println(tip);
+		assertEquals(String.class, tip.getClass());
+	}
 }
